a775f51bca7b289ca9c9b6fb022fc057cf133395
Joey Yu <joey@itsjoeoui.com>
2023-01-27 11:05:36 -0500

    Add all commands
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "awsonbudget/cli/cmd"
+import (
+	"awsonbudget/cli/cmd"
+	_ "awsonbudget/cli/cmd/pod"
+)
 
 func main() {
 	cmd.Execute()
