e62d9b0ef5a529efadc77050756d0b2eeb217c69
Joey Yu <joey@itsjoeoui.com>
2023-02-11 17:03:18 -0500

    Finish node
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

--- cmd/node.go
+++ cmd/node.go
@@ -4,10 +4,36 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 
 	"github.com/spf13/cobra"
 )
+
+type nodeLsResp struct {
+	Status bool   `json:"status"`
+	Msg    string `json:"msg"`
+	Data   []struct {
+		Name   string `json:"name"`
+		Id     string    `json:"id"`
+		Status string   `json:"status"`
+		Pod    struct {
+			Name string `json:"name"`
+			Id   int    `json:"id"`
+		} `json:"pod"`
+	} `json:"data"`
+}
+
+type nodeRegisterResp struct {
+	Status bool   `json:"status"`
+	Msg    string `json:"msg"`
+}
+
+type nodeRmResp struct {
+	Status bool   `json:"status"`
+	Msg    string `json:"msg"`
+}
 
 var nodeCmd = &cobra.Command{
 	Use:   "node",
@@ -32,8 +58,51 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
+	Args: cobra.MaximumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("nodeLs called")
+		// Build the request
+		req, err := http.NewRequest(http.MethodGet, ManagerEp+"/cloud/node/", nil)
+		if err != nil {
+			fmt.Print("Failed: ")
+			fmt.Println(err)
+			return
+		}
+
+		params := req.URL.Query()
+		if len(args) > 0 {
+			params.Add("node_name", args[0])
+		}
+		req.URL.RawQuery = params.Encode()
+
+		// Send the request
+		res, err := Client.Do(req)
+		if err != nil {
+			fmt.Print("Failed: ")
+			fmt.Println(err)
+			return
+		}
+		defer res.Body.Close()
+
+		// Decode the response
+		var response nodeLsResp
+		err = json.NewDecoder(res.Body).Decode(&response)
+		if err != nil {
+			fmt.Print("Failed: ")
+			fmt.Println(err)
+			return
+		}
+
+		// Print the response
+		if response.Status {
+			fmt.Print("Success: ")
+			fmt.Println(response.Msg)
+			for _, node := range response.Data {
+                fmt.Printf("| ID: %s |\n| Name: %s | Status: %s | Pod: %s |\n", node.Id, node.Name, node.Status, node.Pod.Name)
+			}
+		} else {
+			fmt.Print("Failed: ")
+			fmt.Println(response.Msg)
+		}
 	},
 }
 
@@ -46,10 +115,54 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
+    Args: cobra.RangeArgs(1, 2),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("nodeRegister called")
+		// Build the request
+		req, err := http.NewRequest(http.MethodPost, ManagerEp+"/cloud/node/", nil)
+		if err != nil {
+			fmt.Print("Failed: ")
+			fmt.Println(err)
+			return
+		}
+
+		params := req.URL.Query()
+		params.Add("node_name", args[0])
+		if len(args) > 1 {
+			params.Add("pod_name", args[1])
+		}
+
+		req.URL.RawQuery = params.Encode()
+
+		// Send the request
+		res, err := Client.Do(req)
+		if err != nil {
+			fmt.Print("Failed: ")
+			fmt.Println(err)
+			return
+		}
+		defer res.Body.Close()
+
+		// Decode the response
+		var response nodeRegisterResp
+		err = json.NewDecoder(res.Body).Decode(&response)
+		if err != nil {
+			fmt.Print("Failed: ")
+			fmt.Println(err)
+			return
+		}
+
+		// Print the response
+		if response.Status {
+			fmt.Print("Success: ")
+			fmt.Println(response.Msg)
+		} else {
+			fmt.Print("Failed: ")
+			fmt.Println(response.Msg)
+		}
 	},
 }
+
+
 
 var nodeRmCmd = &cobra.Command{
 	Use:   "rm",
@@ -60,10 +173,49 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
+    Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("nodeRm called")
+		// Build the request
+		req, err := http.NewRequest(http.MethodDelete, ManagerEp+"/cloud/node/", nil)
+		if err != nil {
+			fmt.Print("Failed: ")
+			fmt.Println(err)
+			return
+		}
+
+		params := req.URL.Query()
+		params.Add("node_name", args[0])
+		req.URL.RawQuery = params.Encode()
+
+		// Send the request
+		res, err := Client.Do(req)
+		if err != nil {
+			fmt.Print("Failed: ")
+			fmt.Println(err)
+			return
+		}
+		defer res.Body.Close()
+
+		// Decode the response
+		var response podRmResp
+		err = json.NewDecoder(res.Body).Decode(&response)
+		if err != nil {
+			fmt.Print("Failed: ")
+			fmt.Println(err)
+			return
+		}
+
+		// Print the response
+		if response.Status {
+			fmt.Print("Success: ")
+			fmt.Println(response.Msg)
+		} else {
+			fmt.Print("Failed: ")
+			fmt.Println(response.Msg)
+		}
 	},
 }
+
 
 func init() {
 	rootCmd.AddCommand(nodeCmd)
